da675e6ccf72c4b244733d1190d330d726e7fd02
PayamAnari <anari.p62@gmail.com>
2023-09-18 15:47:41 +0200

    update

--- src/fake-data/all-categories.js
+++ src/fake-data/all-categories.js
@@ -1,6 +1,9 @@
-export default [
-  "FAKE: electronics",
-  "FAKE: jewelery",
+const allCategories = [
+  'FAKE: electronics',
+  'FAKE: jewelery',
   "FAKE: men's clothing",
   "FAKE: women's clothing",
 ];
+
+
+export default allCategories;
