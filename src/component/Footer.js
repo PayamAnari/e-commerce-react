ccafd7f8b8ba13d9250d072dc6504e3bd3734496
PayamAnari <anari.p62@gmail.com>
2023-09-18 15:31:14 +0200

    Add footer.js

---
+++ src/component/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer bg-dark">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-12 mb-3">
+              <div className="text-center mb-3">
+                <div className="text-center pt-3 text-light py-2">
+                  &copy; 2023 Payam Anari HYF
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
