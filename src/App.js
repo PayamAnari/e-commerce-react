070db7a25291d539324b56d6642c064ea5123162
PayamAnari <anari.p62@gmail.com>
2023-09-18 15:28:19 +0200

    Add e-commerce-react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import Products from './component/Products';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Products />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
